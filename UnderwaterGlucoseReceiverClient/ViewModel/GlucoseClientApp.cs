e45507ed38a053fff4a2201e265fda164654a208
Corentin Altepe <corentin.altepe@gmail.com>
2016-10-09 19:54:04 +0300

    Created App events for binding with View

--- UnderwaterGlucoseReceiverClient/ViewModel/GlucoseClientApp.cs
+++ UnderwaterGlucoseReceiverClient/ViewModel/GlucoseClientApp.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace UnderwaterGlucoseReceiverClient
 {
-    public class GlucoseClientApp
+    public class GlucoseClientApp : INotifyPropertyChanged
     {
         public IAcousticModem ModemInterface
         { get; set; }
@@ -14,6 +16,8 @@ namespace UnderwaterGlucoseReceiverClient
         // Content of the console to be displayed on the config Tab
         // Updated at every new incoming data from sensor, connection event, etc
         private string console;
+
+
         public string Console
         {
             get { return console; }
@@ -41,13 +45,40 @@ namespace UnderwaterGlucoseReceiverClient
         {
             return DateTime.Now.ToLongTimeString() + ": ";
         }
-
-
+        
         #endregion
 
 
         #region Events
+        // Notify the view that a property has changed (update the view)
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
 
+        // Event generated from the View
+        public ICommand ComPortComboboxSelectionChanged
+        { get { return new DelegateCommand(OnComPortComboboxSelectionChanged, CanComPortComboboxSelectionChange); } }
+
+        private bool CanAlways(object context)
+        {
+            return true;
+        }
+        private bool CanComPortComboboxSelectionChange(object context)
+        {
+            //this is called to evaluate whether FuncToCall can be called
+            //for example you can return true or false based on some validation logic
+            return true;
+        }
+        private void OnComPortComboboxSelectionChanged(object context)
+        {
+            
+        }
         #endregion
 
     }
